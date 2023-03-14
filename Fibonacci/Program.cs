00362c2f85eef4fd1b3ab6a79b07e4777ba1e5d8
alebs10 <alexandre2002.ab@gmail.com>
2023-03-14 12:24:30 -0300

    Sequencia de Fibonacci.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sequência de Fibonnaci");
+            Console.WriteLine("Digite a quantidade de sequência que deseja ver: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int x = 0, y = 1, total;
+
+            for (int i = 0; i < n; i++)
+            {
+                total = x + y;
+                Console.WriteLine("O numero é " + total);
+                y = x;
+                x = total;
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
